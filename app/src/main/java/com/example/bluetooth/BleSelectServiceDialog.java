c2800031cc0de10ab536c8db4c8d21c5e795b002
Mega PC <anrobott@gmail.com>
2023-07-20 21:37:03 +0300

    https://github.com/myGitam/android.git

---
+++ app/src/main/java/com/example/bluetooth/BleSelectServiceDialog.java
@@ -0,0 +1,17 @@
+package com.example.bluetooth;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class BleSelectServiceDialog extends DialogFragment {
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        return builder.setTitle("Диалоговое окно").setMessage("Для закрытия окна нажмите ОК").create();
+    }
+}
