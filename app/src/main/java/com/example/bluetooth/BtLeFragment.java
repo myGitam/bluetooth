834d57aa50107e00b14138e003d38d7d082aebbd
Mega PC <anrobott@gmail.com>
2023-09-20 12:06:11 +0300

    https://github.com/myGitam/android.git

--- app/src/main/java/com/example/bluetooth/BtLeFragment.java
+++ app/src/main/java/com/example/bluetooth/BtLeFragment.java
@@ -226,110 +226,110 @@ public class BtLeFragment extends Fragment {
             Log.d(TAG, "onLongClick: ");
           device=pairedDev;
           //соединяет и получает список сервисов
-            @SuppressLint("MissingPermission") BluetoothGatt gatt = pairedDev.getPairBluDev().connectGatt(getContext(), false,
-                    bluetoothGattCallback, TRANSPORT_LE);
+//            @SuppressLint("MissingPermission") BluetoothGatt gatt = pairedDev.getPairBluDev().connectGatt(getContext(), false,
+//                    bluetoothGattCallback, TRANSPORT_LE);
             //Отображаю меню для выбора сервиса с которым нужно работать это для теста тут наеписано/ нужно переносить в правильное место
-            DialogFragment dialogFragment=new BleSelectServiceDialog(getContext());
+            DialogFragment dialogFragment=new BleSelectServiceDialog(getContext(),pairedDev);
             dialogFragment.show(getParentFragmentManager(),"Select Services");
 
 
         }
     };
 
-     @SuppressLint("NewApi")
-     private final BluetoothGattCallback bluetoothGattCallback=new BluetoothGattCallback() {
-         @SuppressLint("MissingPermission")
-         @Override
-         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-             super.onConnectionStateChange(gatt, status, newState);
-             if(status == GATT_SUCCESS) {
-                 if (newState == BluetoothGatt.STATE_CONNECTED) {
-                     // Connected to the device, start discovering services
-                     //Получаю статус подключения состояние сопряжения
-                     int bondstate = device.getPairBluDev().getBondState();
-                     if(bondstate == BOND_NONE || bondstate == BOND_BONDED) {
-                         // Подключились к устройству, вызываем discoverServices с задержкой
-                         gatt.discoverServices();
-                         }
-
-
-                 } // если пользователь отключил
-                 else if (newState == BluetoothGatt.STATE_DISCONNECTED) {
-                     // Disconnected from the device
-                     Log.d(TAG, "onConnectionStateChange: " + " STATE_DISCONNECTED");
-                     gatt.close();
-                 } else {
-                     // мы или подключаемся или отключаемся, просто игнорируем эти статусы
-                 }
-             }
-             else {
-                 // Произошла ошибка... разбираемся, что случилось!
-                 Log.d(TAG, "Error connect: ");
-                 gatt.close();
-             }
-
-         }
-
-         @SuppressLint("MissingPermission")
-         @Override
-         //Метод для обнаружения сервисов
-         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-             super.onServicesDiscovered(gatt, status);
-             if (status == GATT_FAILURE) {
-                 Log.d(TAG, "Service discovery failed\" "); /// Сервисы не найдены
-                 gatt.close();
-                 return;
-             }
-             Log.d(TAG, "Discovered: "); //сервисы найдены
-             //ниже проверяю все сервисы на наличие характеристик с возможностью чтени NOTIFY и записи WRITE и добавляю их в отдельный списко с которым буду работать уже из всплывающего менюп о длителдьному нажатию
-             if (status == BluetoothGatt.GATT_SUCCESS) {
-                 // Find the service with the given UUID
-                 //получаю список сервисов
-                 final List<BluetoothGattService> services = gatt.getServices(); /// получаю список найденныс сервисов
-
-                 Log.d(TAG, "services: "+ services.size());
-                 int index = 0; ///просто задаю индексацию для себя чтоб понимать какой сервис в лог выводит
-                 //перебираю все сервисы цыклом и сразу в цикле проверяю характеристики на наличие возможности чтения/записи
-                 for (BluetoothGattService s:services){
-                     index++;
-                     Log.d(TAG, "serviceUUID: "+index +": "+s.getUuid());
-
-                     //получаю список характеристик в сервисе
-                     List<BluetoothGattCharacteristic> characteristics = s.getCharacteristics();
-
-                     // в этом цикле проверяю есть ли у сервиса нужные мне хзарактеристики если есть то я этот сервис запишу в отдельный список только сервисов которые поддерживают то что мне нужно
-                     for (BluetoothGattCharacteristic characteristic : characteristics) {
-                         // Получаю UUID характеристики конкретной
-                         UUID value =characteristic.getUuid();
-                         Log.d(TAG, "UUID  value characteristic: "+ value);
-
-                         int property = characteristic.getProperties(); // переменная чтоб понять характеристика для записи или для чтения
-                         if(((property & BluetoothGattCharacteristic.PROPERTY_NOTIFY)>0) && ((property & BluetoothGattCharacteristic.PROPERTY_WRITE|BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE)>0)){
-                            // characteristicsReadWrite.add(characteristic);
-                             Log.d(TAG, "property READ/WRITE add to supportedServices: " );
-                           supportedServices.add(s);
-
-                         }
-                         // проверха характеристики для записи или для чтения
-//                         if ((property & BluetoothGattCharacteristic.PROPERTY_NOTIFY) > 0) {
-//                             Log.d(TAG, "property READ "+ ""+property );
+//     @SuppressLint("NewApi")
+//     private final BluetoothGattCallback bluetoothGattCallback=new BluetoothGattCallback() {
+//         @SuppressLint("MissingPermission")
+//         @Override
+//         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+//             super.onConnectionStateChange(gatt, status, newState);
+//             if(status == GATT_SUCCESS) {
+//                 if (newState == BluetoothGatt.STATE_CONNECTED) {
+//                     // Connected to the device, start discovering services
+//                     //Получаю статус подключения состояние сопряжения
+//                     int bondstate = device.getPairBluDev().getBondState();
+//                     if(bondstate == BOND_NONE || bondstate == BOND_BONDED) {
+//                         // Подключились к устройству, вызываем discoverServices с задержкой
+//                         gatt.discoverServices();
 //                         }
-//                         if ((property & (BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE | BluetoothGattCharacteristic.PROPERTY_WRITE)) > 0) {
-//                             Log.d(TAG, "property: "+ "WRITE"+ property  );
+//
+//
+//                 } // если пользователь отключил
+//                 else if (newState == BluetoothGatt.STATE_DISCONNECTED) {
+//                     // Disconnected from the device
+//                     Log.d(TAG, "onConnectionStateChange: " + " STATE_DISCONNECTED");
+//                     gatt.close();
+//                 } else {
+//                     // мы или подключаемся или отключаемся, просто игнорируем эти статусы
+//                 }
+//             }
+//             else {
+//                 // Произошла ошибка... разбираемся, что случилось!
+//                 Log.d(TAG, "Error connect: ");
+//                 gatt.close();
+//             }
+//
+//         }
+//
+//         @SuppressLint("MissingPermission")
+//         @Override
+//         //Метод для обнаружения сервисов
+//         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+//             super.onServicesDiscovered(gatt, status);
+//             if (status == GATT_FAILURE) {
+//                 Log.d(TAG, "Service discovery failed\" "); /// Сервисы не найдены
+//                 gatt.close();
+//                 return;
+//             }
+//             Log.d(TAG, "Discovered: "); //сервисы найдены
+//             //ниже проверяю все сервисы на наличие характеристик с возможностью чтени NOTIFY и записи WRITE и добавляю их в отдельный списко с которым буду работать уже из всплывающего менюп о длителдьному нажатию
+//             if (status == BluetoothGatt.GATT_SUCCESS) {
+//                 // Find the service with the given UUID
+//                 //получаю список сервисов
+//                 final List<BluetoothGattService> services = gatt.getServices(); /// получаю список найденныс сервисов
+//
+//                 Log.d(TAG, "services: "+ services.size());
+//                 int index = 0; ///просто задаю индексацию для себя чтоб понимать какой сервис в лог выводит
+//                 //перебираю все сервисы цыклом и сразу в цикле проверяю характеристики на наличие возможности чтения/записи
+//                 for (BluetoothGattService s:services){
+//                     index++;
+//                     Log.d(TAG, "serviceUUID: "+index +": "+s.getUuid());
+//
+//                     //получаю список характеристик в сервисе
+//                     List<BluetoothGattCharacteristic> characteristics = s.getCharacteristics();
+//
+//                     // в этом цикле проверяю есть ли у сервиса нужные мне хзарактеристики если есть то я этот сервис запишу в отдельный список только сервисов которые поддерживают то что мне нужно
+//                     for (BluetoothGattCharacteristic characteristic : characteristics) {
+//                         // Получаю UUID характеристики конкретной
+//                         UUID value =characteristic.getUuid();
+//                         Log.d(TAG, "UUID  value characteristic: "+ value);
+//
+//                         int property = characteristic.getProperties(); // переменная чтоб понять характеристика для записи или для чтения
+//                         if(((property & BluetoothGattCharacteristic.PROPERTY_NOTIFY)>0) && ((property & BluetoothGattCharacteristic.PROPERTY_WRITE|BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE)>0)){
+//                            // characteristicsReadWrite.add(characteristic);
+//                             Log.d(TAG, "property READ/WRITE add to supportedServices: " );
+//                           supportedServices.add(s);
+//
 //                         }
-
-
-                     }
-
-                 }
-                 //вывожу списко устройств чтение запись
-                Log.d(TAG, "property READ/WRITE ALL:- "+ " "+ supportedServices.get(1).getUuid());
-
-
-             }
-
-         }
-     };
+//                         // проверха характеристики для записи или для чтения
+////                         if ((property & BluetoothGattCharacteristic.PROPERTY_NOTIFY) > 0) {
+////                             Log.d(TAG, "property READ "+ ""+property );
+////                         }
+////                         if ((property & (BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE | BluetoothGattCharacteristic.PROPERTY_WRITE)) > 0) {
+////                             Log.d(TAG, "property: "+ "WRITE"+ property  );
+////                         }
+//
+//
+//                     }
+//
+//                 }
+//                 //вывожу списко устройств чтение запись
+//                Log.d(TAG, "property READ/WRITE ALL:- "+ " "+ supportedServices.get(1).getUuid());
+//
+//
+//             }
+//
+//         }
+//     };
 
 
 
